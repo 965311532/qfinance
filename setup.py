515c320c3e2be37f0d6ddea9614d8bae793c0a80
965311532 <gabriele.armento@gmail.com>
2022-05-26 20:04:12 +0200

    Refactored

--- setup.py
+++ setup.py
@@ -1,17 +1,18 @@
 # Simple setup file for qfinance
-from distutils.core import setup
+from setuptools import setup, find_packages
 
 setup(
     name="qfinance",
-    version="0.2.4",
+    version="0.2.5",
     description="A simple financial database management package",
     url="https://github.com/965311532/qfinance",
     author="Gabriele Armento",
     author_email="gabriele.armento@gmail.com",
     license="MIT",
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
     install_requires=[
         "pandas",
         "fastparquet",
     ],
-    py_modules=["qfinance", "utils", "metadata"],
 )
